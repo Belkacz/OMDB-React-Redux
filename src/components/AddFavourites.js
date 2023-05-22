6f1deb75b381916508a23b44c6af3cfadbc5b44a
Lukas Belka <70450588+Belkacz@users.noreply.github.com>
2023-05-22 13:38:25 +0200

    Update AddFavourites.js

--- src/components/AddFavourites.js
+++ src/components/AddFavourites.js
@@ -16,7 +16,7 @@ const AddFavourite = ({ movieData }) => {
       >
         Favorite
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           width="16"
           height="16"
           fill="currentColor"
